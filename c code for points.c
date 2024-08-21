9283074bdfbbbee42043bc405696cae4b9a8492c
Vishnu Prasad <131956832+Vishnu51cse@users.noreply.github.com>
2024-08-21 22:52:40 +0530

    Update c code for points.c

--- c code for points.c
+++ c code for points.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define SIZE 1000
-
 void findPeaks(double arr[], int n, int max[], int *maxCount, int min[], int *minCount) {
     *maxCount = 0;
     *minCount = 0;
-    
     for (int i = 1; i < n - 1; i++) {
         if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
             max[*maxCount] = i;
@@ -17,11 +14,9 @@ void findPeaks(double arr[], int n, int max[], int *maxCount, int min[], int *mi
         }
     }
 }
-
 void writeCSV(const char* fileName, double arr[], int max[], int maxCount, int min[], int minCount, int n) {
     FILE *file = fopen(fileName, "w");
     fprintf(file, "Index,Value,PeakType\n");
-    
     for (int i = 0; i < n; i++) {
         int isMax = 0, isMin = 0;
         for (int j = 0; j < maxCount; j++) {
@@ -44,15 +39,12 @@ void writeCSV(const char* fileName, double arr[], int max[], int maxCount, int m
             fprintf(file, "%d,%lf,\n", i, arr[i]);
         }
     }
-    
     fclose(file);
 }
-
 int main() {
     double data1[SIZE], data2[SIZE];
     int max1[SIZE], min1[SIZE], maxCount1, minCount1;
     int max2[SIZE], min2[SIZE], maxCount2, minCount2;
-    
     FILE *file = fopen("Data_1.txt", "r");
     if (file == NULL) {
         printf("Error: Could not open file Data_1.txt\n");
@@ -63,7 +55,6 @@ int main() {
         i++;
     }
     fclose(file);
-    
     file = fopen("Data_2.txt", "r");
     if (file == NULL) {
         printf("Error: Could not open file Data_2.txt\n");
@@ -74,13 +65,10 @@ int main() {
         j++;
     }
     fclose(file);
-    
     findPeaks(data1, i, max1, &maxCount1, min1, &minCount1);
     findPeaks(data2, j, max2, &maxCount2, min2, &minCount2);
-    
     writeCSV("data_1.csv", data1, max1, maxCount1, min1, minCount1, i);
     writeCSV("data_2.csv", data2, max2, maxCount2, min2, minCount2, j);
-    
     printf("CSV files generated.\n");
 
     return 0;
