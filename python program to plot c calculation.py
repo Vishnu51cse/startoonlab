5707eb05337d48831e71d08073d39bea792cffdc
Vishnu Prasad <131956832+Vishnu51cse@users.noreply.github.com>
2024-08-21 22:51:02 +0530

    Update python program to plot c calculation.py

--- python program to plot c calculation.py
+++ python program to plot c calculation.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# Load the CSV files
 data1 = pd.read_csv('/content/data_1.csv')
 data2 = pd.read_csv('/content/data_2.csv')
-
-# Plot for Data 1
 plt.figure(figsize=(10, 6))
 plt.plot(data1['Index'], data1['Value'], label='Signal')
 plt.scatter(data1[data1['PeakType'] == 'Maxima']['Index'], data1[data1['PeakType'] == 'Maxima']['Value'], color='red', label='Maxima')
@@ -15,8 +11,6 @@ plt.xlabel('Index')
 plt.ylabel('Value')
 plt.legend()
 plt.show()
-
-# Plot for Data 2
 plt.figure(figsize=(10, 6))
 plt.plot(data2['Index'], data2['Value'], label='Signal')
 plt.scatter(data2[data2['PeakType'] == 'Maxima']['Index'], data2[data2['PeakType'] == 'Maxima']['Value'], color='red', label='Maxima')
