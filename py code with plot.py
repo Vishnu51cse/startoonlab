3ae9f789135d591b17945cc4de5b9810c81d0182
Vishnu Prasad <131956832+Vishnu51cse@users.noreply.github.com>
2024-08-21 22:50:07 +0530

    Update and rename py code with plot to py code with plot.py

--- py code with plot.py
+++ py code with plot.py
@@ -1,28 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
 data1 = np.loadtxt('/content/Data_1.txt') 
 data2 = np.loadtxt('/content/Data_2.txt')
-
 def find_peaks(values):
     max_vals = []
     min_vals = []
-    
-    # Find maxima and minima
     for i in range(1, len(values) - 1):
         if values[i] > values[i - 1] and values[i] > values[i + 1]:
             max_vals.append(i)
         elif values[i] < values[i - 1] and values[i] < values[i + 1]:
             min_vals.append(i)
-    
     return max_vals, min_vals
-
-
 max1, min1 = find_peaks(data1)
 max2, min2 = find_peaks(data2)
-
-
 plt.figure(figsize=(10, 6))
 plt.plot(data1, label='Data 1')
 plt.scatter(max1, data1[max1], color='red', label='Max', zorder=5)
@@ -30,8 +20,6 @@ plt.scatter(min1, data1[min1], color='blue', label='Min', zorder=5)
 plt.title('Data 1 with Maxima and Minima')
 plt.legend()
 plt.show()
-
-
 plt.figure(figsize=(10, 6))
 plt.plot(data2, label='Data 2')
 plt.scatter(max2, data2[max2], color='red', label='Max', zorder=5)
